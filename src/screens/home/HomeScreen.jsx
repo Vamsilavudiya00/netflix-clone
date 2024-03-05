1eacc63e5c8d2e08c2a5e8ba6b47daf477f6de1d
lavudiya vamsi <vamsilavadiyaa@gmail.com>
2024-03-05 22:00:29 +0530

    added link

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -4,85 +4,120 @@ import NavBar from "../../components/NavBar";
 import HeaderCarousel from "../../components/HeaderCarousel";
 
 const HomeScreen = () => {
+  const linkYT = "https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg";
+  const linkyt1 = "https://youtu.be/q3u6c1DR9-4";
   return (
     <div>
       <NavBar />
       <HeaderCarousel />
       <div>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
         <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
-        ></iframe>
-        <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
-        ></iframe>
-        <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
-        ></iframe>
-        <iframe
-          width="420"
-          height="315"
-          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+          width="300"
+          height="200"
+          src="https://www.youtube.com/embed/q3u6c1DR9-4"
+          title="NEFFEX   My Way"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
         ></iframe>
       </div>
     </div>
