be57bd05db38f611a58a8061dd6f882985574155
lavudiya vamsi <vamsilavadiyaa@gmail.com>
2024-03-05 21:51:26 +0530

    added youtube videos

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,15 +1,92 @@
-import React from 'react'
-import './homescreen.css'
-import NavBar from '../../components/NavBar'
-import HeaderCarousel from '../../components/HeaderCarousel'
+import React from "react";
+import "./homescreen.css";
+import NavBar from "../../components/NavBar";
+import HeaderCarousel from "../../components/HeaderCarousel";
 
 const HomeScreen = () => {
   return (
     <div>
-        <NavBar />
-        <HeaderCarousel />
+      <NavBar />
+      <HeaderCarousel />
+      <div>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://youtu.be/q3u6c1DR9-4?si=fmA2JlxRt-waMPjg"
+        ></iframe>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
