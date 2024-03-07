951f036b1a541d1a878d6bd42362aefd376d7c2d
lavudiya vamsi <vamsilavadiyaa@gmail.com>
2024-03-07 12:56:49 +0530

    added carts

---
+++ src/components/OneSection.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "../styles/cart.css";
+
+const OneSection = () => {
+  return (
+    <div className="one_section_carts">
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+      <div className="one_section_cart">
+        <span>Hello</span>
+      </div>
+    </div>
+  );
+};
+
+export default OneSection;
---
+++ src/components/SectionCart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import OneSection from './OneSection'
+import '../styles/sectioncart.css'
+
+const SectionCart = () => {
+  return (
+    <div className='section_cart'>
+        <h3>Trending Lists</h3>
+        <OneSection />
+    </div>
+  )
+}
+
+export default SectionCart
